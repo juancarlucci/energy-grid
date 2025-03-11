6dcd7b3cfc39298c3feb1be9f2ecd4d4236aec68
juancarlucci <juancarloscollins@gmail.com>
2025-03-11 10:28:44 -0700

    Add Apollo Client and basic GraphQL query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+} from "@apollo/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Initialize Apollo Client
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://graphqlzero.almansi.me/api", // Mock GraphQL API
+  }),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
