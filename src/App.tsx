3b1137c44730dfd94d8674d56ba314083f0b3e13
juancarlucci <juancarloscollins@gmail.com>
2025-03-12 16:48:25 -1000

    feat(App.tsx): Include seconds in timestamp formatting for improved readability

--- src/App.tsx
+++ src/App.tsx
@@ -84,6 +84,7 @@ function App() {
     new Date(timestamp).toLocaleTimeString([], {
       hour: "2-digit",
       minute: "2-digit",
+      second: "2-digit",
     });
 
   //* Optimize rendering with useMemo
