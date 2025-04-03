eb873b3505bd2249da5d7269cbaa36500340c2fd
juancarlucci <juancarloscollins@gmail.com>
2025-04-03 19:59:49 +0200

    feat(config): enhance build process with compression and visualization plugins. Enhance performance, accessibility, SEO, and best practices

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: "./postcss.config.cjs",
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: "brotliCompress",
+      ext: ".br",
+      threshold: 1024,
+    }),
+    visualizer({
+      open: true,
+      filename: "dist/stats.html",
+    }),
+  ],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom"],
+          "apollo-client": ["@apollo/client"],
+          "chart-js": ["chart.js", "chartjs-plugin-annotation"],
+        },
+      },
+    },
   },
 });
