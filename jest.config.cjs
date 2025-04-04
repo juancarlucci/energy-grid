d2f1675377deac338d723b857e63a73c8234ad6c
Juan Carlos <juancarlucci@users.noreply.github.com>
2025-04-04 12:23:22 +0200

    Delete jest.config.cjs

--- jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.cjs"],
-  moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  },
-  moduleDirectories: ["node_modules", "src"], // Help Jest find modules in src
-};
